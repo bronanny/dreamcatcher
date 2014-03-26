31a0e88f1672a5dd4fd92401267dc8bdef9a57f2
Simon Forman <simon@mydream.com>
2014-03-26 20:31:47 +0000

    Only serve static content if asked.
    Fixes #6

--- whisper_server.py
+++ whisper_server.py
@@ -1,24 +1,36 @@
 from json import dumps
 from os import chdir, walk, sep, getcwd
-from os.path import join
+from os.path import join, splitext
 from struct import unpack
 from whisper import __readHeader, pointSize, pointFormat
+
+
+STATIC = 'o/'
+N = len(STATIC)
+CONTENT_TYPES = dict(
+  css='text/css',
+  html='text/html',
+  js='application/javascript',
+  )
+
+
+ext_of = lambda filename: splitext(filename)[1][1:]
 
 
 def extract_archives(fh):
   header = __readHeader(fh)
   for archive in header['archives']:
-    n = archive['points']
+    n = pointSize * archive['points']
     fh.seek(archive['offset'])
-    points = fh.read(n * pointSize)
+    points = fh.read(n)
     a = []
-    for i in xrange(0, n * pointSize, pointSize):
+    for i in xrange(0, n, pointSize):
       packedPoint = points[i:i + pointSize]
       timestamp, value = unpack(pointFormat, packedPoint)
       a.append((timestamp, value or 0))
     a.sort(key=lambda p: p[0])
     archive['most_recent'] = a[-1][0] if a else -1
-    archive['oldest'] = a[0][0] if a else -1
+    archive['oldest'] = a[1][0] if a else -1
     archive['points'] = [p for p in a if p[1] is not None]
   return header
 
@@ -41,36 +53,64 @@ def walk_whisper(path):
   return data
 
 
+def collect_static_files(content_path, acceptable_extensions=CONTENT_TYPES):
+  files = {}
+  for dirpath, dirs, filenames in walk(content_path):
+    d = dirpath[len(content_path):].strip(sep)
+    for filename in filenames:
+      if ext_of(filename) in acceptable_extensions:
+        files[join(d, filename)] = join(dirpath, filename)
+  return files
+
+
 def static(resource_path, start_response):
   start_response('200 OK', [('Content-type', ctype(resource_path))])
   return open(resource_path, 'rb')
 
 
-def ctype(path, types=dict(
-  css='text/css',
-  html='text/html',
-  js='application/javascript',
-  )):
-  return types[path.rpartition('.')[2]]
+def ctype(path, types=CONTENT_TYPES):
+  return types[ext_of(path)]
 
 
-def serve_path(content_path, whisper_path):
+def serve_path(whisper_path, content_path=None):
   data = walk_whisper(whisper_path)
   for stat in data:
     data[stat] = dumps(data[stat])
   stat_names = dumps({'stat_names': sorted(data)})
 
-  def whisper_app(environ, start_response):
-    stat_name = environ['PATH_INFO'].strip('/')
-    if stat_name.startswith('o/'):
-      resource = join(content_path, stat_name[2:])
-      return static(resource, start_response)
+  def serve_stat(stat_name, start_response):
     res = data.get(stat_name, '{}') if stat_name else stat_names
     start_response('200 OK', [('Content-type', 'application/json')])
     return [res]
 
+  if content_path:
+    content_path = join(content_path, 'site_content')
+    static_files = collect_static_files(content_path)
+
+    def whisper_app(environ, start_response):
+      stat_name = environ['PATH_INFO'].strip('/')
+      if stat_name.startswith(STATIC):
+        resource_path = static_files.get(stat_name[N:])
+        if not resource_path:
+          start_response('404 NOT FOUND', [('Content-type', 'text/html')])
+          return ['404 NOT FOUND']
+        return static(resource_path, start_response)
+      return serve_stat(stat_name, start_response)
+
+  else: # Don't serve static content.
+
+    def whisper_app(environ, start_response):
+      stat_name = environ['PATH_INFO'].strip('/')
+      return serve_stat(stat_name, start_response)
+
   return whisper_app
 
+
+##from pprint import pprint
+##pprint(collect_static_files(
+##  'C:\\Users\\Jesse\\Desktop\\vag\\dreamcatcher\\dreamcatcher'
+##  '\\site_content')
+##)
 
 if __name__ == '__main__':
   from argparse import ArgumentParser
@@ -84,7 +124,6 @@ if __name__ == '__main__':
   parser.add_argument(
     '-c', '--content_path',
     help='Path to the directory containing the site_content directory.',
-    default='.',
     )
   parser.add_argument(
     '-I', '--IP',
@@ -99,8 +138,8 @@ if __name__ == '__main__':
     )
   args = parser.parse_args()
 
-  content_path = join(args.content_path, 'site_content')
-  wsgi_app = serve_path(content_path, args.whisper_path)
+  wsgi_app = serve_path(args.whisper_path, args.content_path)
   httpd = make_server(args.IP, args.port, wsgi_app)
   print "Serving on port 8000..."
   httpd.serve_forever()
+
